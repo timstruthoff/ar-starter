30091d22ea32648a716175f9cf1677a60245515d
Tim Struthoff <tim.struthoff@gmail.com>
2017-07-12 14:09:36 +0200

    porting AR.js to ES6

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import domready from 'domready';
 
 import Manager from './core/Manager';
 
+import * as AR from 'ar.js';
+
+console.log("______________");
+console.log(AR);
+console.log("______________");
 
 domready(() => {
 
